554e78749964263f41172611180aa2ae97eff64f
momin morgan <mominmorgan10@gmail.com>
2023-06-10 10:54:36 -0700

    reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    l_len = len(my_list) - 1
+
+    while(l_len >= 0):
+        print("{}".format(my_list[l_len]))
+        l_len -= 1
