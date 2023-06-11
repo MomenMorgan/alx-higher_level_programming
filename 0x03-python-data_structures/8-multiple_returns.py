e55ab42dc96ac53f4ff6e5a4f5c0121169b15f66
momin morgan <mominmorgan10@gmail.com>
2023-06-11 03:05:17 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     new_s = (len(sentence), )
-    if len == 0:
+    if len(sentence) == 0:
         return (new_s + (None,))
     else:
         return(new_s + (sentence[0],))
