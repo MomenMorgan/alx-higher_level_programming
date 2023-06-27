893f1b7c29e7f2c3ead3dddb1cdb09f8f9417a14
Momen Morgan <mominmorgan10@gmail.com>
2023-06-27 19:32:52 +0000

    new aqure

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """introduce a square."""
+
+    def __init__(self, size):
+        """Initialize new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
