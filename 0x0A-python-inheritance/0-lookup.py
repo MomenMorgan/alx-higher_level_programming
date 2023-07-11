8460398f86aa32557f1252274cba25d91ae1ec3e
Momen Morgan <mominmorgan10@gmail.com>
2023-07-11 00:03:12 +0000

    hot fix

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+=============================
+look up method
+=============================
+"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
