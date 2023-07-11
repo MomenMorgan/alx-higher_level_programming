24733647a34afd47f8e3236a93d42282592dfaa5
Momen Morgan <mominmorgan10@gmail.com>
2023-07-11 00:23:19 +0000

    updating classes

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+===================================
+class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+
+    @classmethod
+    def area(self):
+
+        raise Exception("area() is not implemented")
