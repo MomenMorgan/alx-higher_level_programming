1cd1adbbb6954a6f18e2861dcda13aad66650b18
momin morgan <mominmorgan10@gmail.com>
2023-08-15 15:07:39 -0700

    add

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,3 @@
 -- creates table
 -- First table to be created
-CREATE TABLE IF NOT EXITST first_table (
-id INT,
-name VARCHAR(256));
+CREATE TABLE IF NOT EXITST first_table (id INT, name VARCHAR(256));
