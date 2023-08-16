a14c3492bb3fbd217c4d1d0907f6ec9a2340ba9f
momin morgan <mominmorgan10@gmail.com>
2023-08-15 17:49:51 -0700

    count

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all scores <= 5 in second_table
+DELETE FROM second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- computes average score of all score records
+SELECT AVG(score) AS average FROM second_table;
