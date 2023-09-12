14846c330fa4ca189d7b6c4dd1427dfe80f660cd
momin morgan <mominmorgan10@gmail.com>
2023-09-12 16:14:48 -0700

    hot fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,12 +5,13 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-      this.print = function () {
-        for (let i = 0; i < this.height; i++) {
-          console.log('X'.repeat(this.width));
-        }
-      };
     }
   }
+
+  print = function () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  };
 }
 exports.Rectangle = Rectangle;
