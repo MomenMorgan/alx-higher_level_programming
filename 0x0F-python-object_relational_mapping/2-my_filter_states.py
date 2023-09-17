cdf320b790efade14d29f8605ba6d5529558d9bb
momin morgan <mominmorgan10@gmail.com>
2023-09-17 10:45:59 -0700

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,25 +2,27 @@
 import MySQLdb
 import sys
 
-"""script that takes in an argument and displays all values 
-in the states table of hbtn_0e_0_usa where name matches the argument."""
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 
-if __name__ == "__main__":
-        """Initialize connection"""
-        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-        """Create a cursor"""
-        cur = db.cursor()
-        """Executes query"""
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                        ORDER BY states.id ASC".format(sys.argv[4]))
-        """Obtain all records"""
-        rows = cur.fetchall()
-        """Print all records"""
-        for row in rows:
-                print(row)
-        """Close cursor"""
-        cur.close()
-        """Close connection"""
-        db.close()
-      
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
