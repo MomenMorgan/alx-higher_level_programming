0ac3ccebd29464c0306f704bd65fcc3b9145fe9f
momin morgan <mominmorgan10@gmail.com>
2023-09-28 15:47:40 -0700

    hot fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """ max number process"""
     max_n = None
     for i in list_of_integers:
         if max_n is None or max_n < i:
