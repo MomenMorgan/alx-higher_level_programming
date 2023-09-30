0d2ebdc5bad827c5a95a6f88d611b3935437d039
momin morgan <mominmorgan10@gmail.com>
2023-09-29 18:04:36 -0700

    add

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,6 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = sys.argv[2]
 
     data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
     send = requests.post(url, data=data)
