8efc77b350fd6afbddd12990fd6e71280f595485
momin morgan <mominmorgan10@gmail.com>
2023-09-29 17:12:59 -0700

    email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL
+with the email as a parameter with requests"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {"email": email}
+    response = requests.post(url, data=data)
+
+    print(response.text)
