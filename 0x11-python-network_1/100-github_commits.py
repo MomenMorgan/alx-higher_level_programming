1a365b3386366d3f92d0d98bae7b481ee670bafc
momin morgan <mominmorgan10@gmail.com>
2023-09-29 18:22:25 -0700

    commitss

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""python script to show  10 commits takes tow args username ans pass
+"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    commits = get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
