cdd0886bb7f13287532882ed9ebae6c0cd2fbd0c
momin morgan <mominmorgan10@gmail.com>
2023-10-24 11:28:30 -0700

    hot fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 
 // script that writes a string to a file.
 
+const write = require('fs');
+
 const file = process.argv[2];
 const text = process.argv[3];
-
-const write = require('fs');
 
 write.writeFile(file, text, 'utf8', (err) => {
   if (err) {
